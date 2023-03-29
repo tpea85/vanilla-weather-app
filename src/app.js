116f269bd2d203c414244cbf086271c24ce93ca2
tpea85 <every1moves@gmail.com>
2023-03-29 17:10:33 -0400

    API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-alert ("Hello World")
+let apiKey="392fecb7049a8a39ff68de93bcb7e6a7";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid={API key}`;
+
+console.log(apiUrl);
+
