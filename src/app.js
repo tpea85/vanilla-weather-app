ef34e17821ef9e0362afbaf54f4b06906cd83780
tpea85 <every1moves@gmail.com>
2023-03-30 23:57:11 -0400

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
+function displayTemperature(response) {
+    let temperatureElement=document.querySelector("#temperature");
+     let cityElement=document.querySelector("#city");
+      let descriptionElement=document.querySelector("#description");
+      let humidityElement=document.querySelector("#humidity");
+      let windElement=document.querySelector("#wind");
+    temperatureElement.innerHTML=Math.round
+    (response.data.main.temp);
+    cityElement.innerHTML=response.data.name;
+     descriptionElement.innerHTML=response.data.weather[0].description;
+     humidityElement.innerHTML=response.data.main.humidity;
+     windElement.innerHTML=Math.round(response.data.wind.speed);
+}
+
 let apiKey="392fecb7049a8a39ff68de93bcb7e6a7";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid={API key}`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 
+axios.get(apiUrl).then(displayTemperature);
