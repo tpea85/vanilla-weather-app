7ea46abd0cb1abc9777e91b0022412b12d735dbe
tpea85 <every1moves@gmail.com>
2023-04-03 07:45:26 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -45,7 +45,19 @@ function displayTemperature(response) {
 iconElement.setAttribute("alt", response.data.weather[0].description);
     }
 
-let apiKey="392fecb7049a8a39ff68de93bcb7e6a7";
-let city="New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+    let apiKey="392fecb7049a8a39ff68de93bcb7e6a7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
