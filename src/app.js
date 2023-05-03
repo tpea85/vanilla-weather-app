e8ace8fe513550b7d3d1b6adefcbadbcc794ed3d
tpea85 <every1moves@gmail.com>
2023-05-03 14:07:57 -0400

    Removed the imperial conversion

--- src/app.js
+++ src/app.js
@@ -127,9 +127,6 @@ let celsiusTemperature=null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink=document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
 let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
